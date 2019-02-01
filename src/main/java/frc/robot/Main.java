f2955a0acb039ee616c06a58443bc5063006260f
Nathan Leach <nleach999@gmail.com>
2019-02-01 17:08:22 -0600

    breaking build for test

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -17,6 +17,6 @@ public final class Main {
    * <p>If you change your main robot class, change the parameter type.
    */
   public static void main(String... args) {
-    RobotBase.startRobot(Robot::new);
+    RobotBase.startRobot123(Robot::new);
   }
 }
