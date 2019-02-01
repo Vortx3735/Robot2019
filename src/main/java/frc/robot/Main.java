1c2e99b140011c94e0b7a41998cc3cfceb3af091
Nathan Leach <nleach999@gmail.com>
2019-02-01 17:37:27 -0600

    fix

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -17,6 +17,6 @@ public final class Main {
    * <p>If you change your main robot class, change the parameter type.
    */
   public static void main(String... args) {
-    RobotBase.startRobot123(Robot::new);
+    RobotBase.startRobot(Robot::new);
   }
 }
