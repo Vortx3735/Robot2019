e93baa30ed8ee6eab351b2d62aaa161015b3c1c2
Joseph Martinez <jtm5317@gmail.com>
2019-03-20 11:49:04 -0500

    Fixed the arduino update method
    Should allow the data to be pushed on a string and then data obtained from the last occurrence of the decimal pattern

--- src/main/java/frc/robot/subsystems/ArduinoCo.java
+++ src/main/java/frc/robot/subsystems/ArduinoCo.java
@@ -7,24 +7,25 @@
 
 package frc.robot.subsystems;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * Add your docs here.
- */
 public class ArduinoCo extends Subsystem {
 
   double distance;
   public static SerialPort sp;
-  int count;
-  String builder;
+  Pattern p;
+  Matcher m;
+  String s;
 
   public ArduinoCo() {
     sp = new SerialPort(9600, Port.kUSB);
-    builder = "";
+    s = "";
+    p = Pattern.compile("[0-9]{3}\\.[0-9]{2}");
   }
 
   public double getDistance() {
@@ -32,33 +33,20 @@ public class ArduinoCo extends Subsystem {
   }
 
   public void update() {
-    // System.out.println("Update was called");
-    count++;
+
     try {
       if (sp.getBytesReceived() > 0) {
-        builder += sp.readString();
-      }
-      if(builder.contains("[0-9]{3}.[0-9]{2}")) {
-        
-      }
-      if(count%8==0) {
-        // int first = builder.indexOf("\n");
-        // int last = builder.lastIndexOf("\n");
-        // if(first!=last && last > first) {
-        //   String data = builder.substring(builder.indexOf("\n"), builder.lastIndexOf("\n")).trim();
-        //   if(data.length()==5) {
-        //     System.out.println(data);
-        //   } else {
-        //     data = data.substring(0, data.indexOf("\n"));
-        //     if(data.length()==5) {
-        //       System.out.println(data);
-        //     }
-        //   }
-        System.out.println(builder);
-          
-          builder = "";
-        
-
+        s += sp.readString().trim(); //add the characters that were read in
+        m = p.matcher(s);
+        if(s.length()>=6&&m.find()) { //if the current string has length greater than 6 and contains the pattern
+          for (int i = s.length()-1; i >=0; i-=6) { //loop backwards from end to beginning to find last occurrence
+                if(m.find(Math.max(i, 0))) {        //if it finds it from index i or after
+                    distance = Double.parseDouble(s.substring(m.start(), m.end()))); //set the distance
+                    s = s.substring(m.end());       //remove the string from the end of the last data and before
+                    break;                          //break out of the loop
+                }
+            }
+        }    
       }
 
     } catch (Exception e) {
